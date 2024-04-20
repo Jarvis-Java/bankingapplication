190f8622d07e8892c60fff6599e3965288269f8f
Amit Vishwakarma <amitamitamit111@gmail.com>
2024-04-20 17:19:46 +0530

    service discovery conf

---
+++ service-discovery/src/test/java/com/bankingapplication/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankingapplication.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
