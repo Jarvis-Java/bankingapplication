1adddcceb9009b1144f3360ec208f300de978720
Amit Vishwakarma <amitamitamit111@gmail.com>
2024-04-25 16:31:59 +0530

    added rest client.

---
+++ api-gateway/src/main/java/com/bankingapplication/apigateway/config/RestClientConfiguration.java
@@ -0,0 +1,20 @@
+package com.bankingapplication.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestClientConfiguration {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+    
+    @Bean
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
+}
